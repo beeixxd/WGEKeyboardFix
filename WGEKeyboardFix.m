6d508ce8c086b0b71b65c5d54b9fccba24743f04
beeieed <beeieed@hotmail.com>
2026-07-12 06:23:21 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,35 +1,52 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 
-static void forceDismissKeyboard(void) {
+static BOOL g_logicOverrideActive = NO;
+
+static BOOL (*orig_becomeFirstResponder)(id, SEL);
+static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
+    if (g_logicOverrideActive) {
+        return NO;
+    }
+    return orig_becomeFirstResponder(self, _cmd);
+}
+
+static void (*orig_makeKeyWindow)(id, SEL);
+static void new_makeKeyWindow(id self, SEL _cmd) {
+    if (g_logicOverrideActive) {
+        return;
+    }
+    orig_makeKeyWindow(self, _cmd);
+}
+
+static void (*orig_becomeKeyWindow)(id, SEL);
+static void new_becomeKeyWindow(id self, SEL _cmd) {
+    if (g_logicOverrideActive) {
+        return;
+    }
+    orig_becomeKeyWindow(self, _cmd);
+}
+
+static id (*orig_placementUndocked)(id, SEL, CGFloat);
+static id new_placementUndocked(id self, SEL _cmd, CGFloat height) {
+    if (g_logicOverrideActive) {
+        return nil;
+    }
+    return orig_placementUndocked(self, _cmd, height);
+}
+
+static void performIronCladCleanup(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIWindow *keyWindow = nil;
-        NSArray *windows = [[UIApplication sharedApplication] windows];
+        [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
         
-        for (UIWindow *window in windows) {
-            if (window.isKeyWindow) {
-                keyWindow = window;
-                break;
-            }
-        }
-        if (!keyWindow && windows.count > 0) {
-            keyWindow = [windows firstObject];
-        }
-        
-        if (keyWindow) {
-            [keyWindow endEditing:YES];
-        }
-        
-        for (UIWindow *window in windows) {
-            NSString *windowName = NSStringFromClass([window class]);
-            if ([windowName containsString:@"TextEffectsWindow"] || [windowName containsString:@"Keyboard"]) {
+        for (UIWindow *window in [[UIApplication sharedApplication] windows]) {
+            NSString *name = NSStringFromClass([window class]);
+            if ([name containsString:@"TextEffects"] || [name containsString:@"Keyboard"]) {
                 window.hidden = YES;
                 window.alpha = 0.0;
-                
-                NSArray *subviews = [window subviews];
-                for (UIView *subview in subviews) {
+                for (UIView *subview in [window subviews]) {
                     NSString *subName = NSStringFromClass([subview class]);
-                    if ([subName containsString:@"Dimming"] || [subName containsString:@"Shadow"]) {
+                    if ([subName containsString:@"Dimming"] || [subName containsString:@"Shadow"] || [subName containsString:@"Corner"]) {
                         subview.hidden = YES;
                         [subview removeFromSuperview];
                     }
@@ -39,36 +56,71 @@ static void forceDismissKeyboard(void) {
     });
 }
 
-@interface WGEKeyboardObserver : NSObject
+@interface WGEKeyboardOverrideObserver : NSObject
 @end
 
-@implementation WGEKeyboardObserver
+@implementation WGEKeyboardOverrideObserver
 
 + (void)load {
-    static WGEKeyboardObserver *observer = nil;
+    static WGEKeyboardOverrideObserver *observer = nil;
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
-        observer = [[WGEKeyboardObserver alloc] init];
+        observer = [[WGEKeyboardOverrideObserver alloc] init];
+        
+        Class viewClass = [UIView class];
+        Method m1 = class_getInstanceMethod(viewClass, @selector(becomeFirstResponder));
+        if (m1) {
+            orig_becomeFirstResponder = (void *)method_getImplementation(m1);
+            method_setImplementation(m1, (IMP)new_becomeFirstResponder);
+        }
+        
+        Class windowClass = [UIWindow class];
+        Method m2 = class_getInstanceMethod(windowClass, @selector(makeKeyWindow));
+        if (m2) {
+            orig_makeKeyWindow = (void *)method_getImplementation(m2);
+            method_setImplementation(m2, (IMP)new_makeKeyWindow);
+        }
+        
+        Method m3 = class_getInstanceMethod(windowClass, @selector(becomeKeyWindow));
+        if (m3) {
+            orig_becomeKeyWindow = (void *)method_getImplementation(m3);
+            method_setImplementation(m3, (IMP)new_becomeKeyWindow);
+        }
+        
+        Class placementClass = objc_getClass("UIInputViewSetPlacementUndocked");
+        if (placementClass) {
+            Method m4 = class_getClassMethod(placementClass, objc_getSelector("placementWithUndockedHeight:"));
+            if (m4) {
+                orig_placementUndocked = (void *)method_getImplementation(m4);
+                method_setImplementation(m4, (IMP)new_placementUndocked);
+            }
+        }
         
         [[NSNotificationCenter defaultCenter] addObserver:observer
-                                                 selector:@selector(handleAppActive)
+                                                 selector:@selector(triggerOverrideMode)
                                                      name:UIApplicationWillEnterForegroundNotification
                                                    object:nil];
                                                    
         [[NSNotificationCenter defaultCenter] addObserver:observer
-                                                 selector:@selector(handleAppActive)
+                                                 selector:@selector(triggerOverrideMode)
                                                      name:UIApplicationDidBecomeActiveNotification
                                                    object:nil];
     });
 }
 
-- (void)handleAppActive {
-    forceDismissKeyboard();
-    for (int i = 1; i <= 4; i++) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            forceDismissKeyboard();
+- (void)triggerOverrideMode {
+    g_logicOverrideActive = YES;
+    performIronCladCleanup();
+    
+    for (int i = 1; i <= 6; i++) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.08 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            performIronCladCleanup();
         });
     }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.6 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        g_logicOverrideActive = NO;
+    });
 }
 
 @end
