a909d84af32e7f42c8c357a590d7073351488d96
beeieed <beeieed@hotmail.com>
2026-07-12 08:59:03 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,108 +1,168 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
+#import <objc/message.h>
 
-static BOOL g_isAppTransitionActive = NO;
+static BOOL gWGEPasscodeRecentlyShown = NO;
 
-static BOOL (*orig_becomeFirstResponder)(id, SEL);
-static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
-    NSString *className = NSStringFromClass([self class]);
-    
-    if ([className containsString:@"Passcode"] || 
-        [className containsString:@"PIN"] || 
-        [className containsString:@"Secure"] || 
-        [className containsString:@"Password"] || 
-        [className containsString:@"Field"]) {
-        return orig_becomeFirstResponder(self, _cmd);
-    }
-    
-    if (g_isAppTransitionActive) {
-        return NO;
-    }
-    
-    return orig_becomeFirstResponder(self, _cmd);
-}
-
-static void (*orig_viewWillDisappear)(id, SEL, BOOL);
-static void new_viewWillDisappear(id self, SEL _cmd, BOOL animated) {
-    orig_viewWillDisappear(self, _cmd, animated);
-    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
-}
-
-static void executeIronCladCleanup(void) {
-    g_isAppTransitionActive = YES;
-    
-    dispatch_async(dispatch_get_main_queue(), ^{
-        [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
-        
-        NSArray *windows = [[UIApplication sharedApplication] windows];
-        for (UIWindow *window in windows) {
-            NSString *name = NSStringFromClass([window class]);
-            if ([name containsString:@"TextEffects"] || [name containsString:@"Keyboard"]) {
-                window.hidden = YES;
-                window.alpha = 0.0;
-                
-                NSArray *subviews = [window subviews];
-                for (UIView *subview in subviews) {
-                    NSString *subName = NSStringFromClass([subview class]);
-                    if ([subName containsString:@"Dimming"] || [subName containsString:@"Shadow"] || [subName containsString:@"Corner"]) {
-                        subview.hidden = YES;
-                        [subview removeFromSuperview];
-                    }
-                }
+static NSArray<UIWindow *> *WGEAllWindows(void) {
+    NSMutableArray<UIWindow *> *result = [NSMutableArray array];
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
+            UIWindowScene *ws = (UIWindowScene *)scene;
+            for (UIWindow *w in ws.windows) {
+                if (w) [result addObject:w];
             }
         }
-    });
-}
-
-@interface WGEKeyboardPerfectFixer : NSObject
-@end
-
-@implementation WGEKeyboardPerfectFixer
-
-+ (void)load {
-    static WGEKeyboardPerfectFixer *fixer = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        fixer = [[WGEKeyboardPerfectFixer alloc] init];
-        
-        Class viewClass = [UIView class];
-        Method m1 = class_getInstanceMethod(viewClass, @selector(becomeFirstResponder));
-        if (m1) {
-            orig_becomeFirstResponder = (void *)method_getImplementation(m1);
-            method_setImplementation(m1, (IMP)new_becomeFirstResponder);
-        }
-        
-        Class vcClass = [UIViewController class];
-        Method m2 = class_getInstanceMethod(vcClass, @selector(viewWillDisappear:));
-        if (m2) {
-            orig_viewWillDisappear = (void *)method_getImplementation(m2);
-            method_setImplementation(m2, (IMP)new_viewWillDisappear);
-        }
-        
-        NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-        
-        [center addObserver:fixer selector:@selector(onLockOrBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
-        [center addObserver:fixer selector:@selector(onLockOrBackground) name:UIApplicationWillResignActiveNotification object:nil];
-        
-        [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationWillEnterForegroundNotification object:nil];
-        [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationDidBecomeActiveNotification object:nil];
-    });
-}
-
-- (void)onLockOrBackground {
-    executeIronCladCleanup();
-}
-
-- (void)onUnlockOrActive {
-    executeIronCladCleanup();
-    for (int i = 1; i <= 5; i++) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            executeIronCladCleanup();
-        });
     }
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.4 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        g_isAppTransitionActive = NO;
+    if (result.count == 0) {
+        NSArray *legacyWindows = [UIApplication sharedApplication].windows;
+        if (legacyWindows) [result addObjectsFromArray:legacyWindows];
+    }
+    return result;
+}
+
+static UIWindow *WGEKeyWindow(void) {
+    for (UIWindow *w in WGEAllWindows()) {
+        if (w.isKeyWindow) return w;
+    }
+    return [UIApplication sharedApplication].keyWindow;
+}
+
+static void WGEPostSyntheticKeyboardHide(void) {
+    CGRect screenBounds = [UIScreen mainScreen].bounds;
+    CGRect endFrame = CGRectMake(0, screenBounds.size.height, screenBounds.size.width, 0);
+    NSValue *frameValue = [NSValue valueWithCGRect:endFrame];
+    NSDictionary *userInfo = @{
+        UIKeyboardFrameBeginUserInfoKey: frameValue,
+        UIKeyboardFrameEndUserInfoKey: frameValue,
+        UIKeyboardAnimationDurationUserInfoKey: @(0.25),
+        UIKeyboardAnimationCurveUserInfoKey: @(UIViewAnimationCurveEaseInOut),
+        UIKeyboardIsLocalUserInfoKey: @YES,
+    };
+    [[NSNotificationCenter defaultCenter] postNotificationName:UIKeyboardWillHideNotification
+                                                          object:nil
+                                                        userInfo:userInfo];
+    [[NSNotificationCenter defaultCenter] postNotificationName:UIKeyboardDidHideNotification
+                                                          object:nil
+                                                        userInfo:userInfo];
+}
+
+static void WGEHideLeftoverPasscodeWindows(void) {
+    UIWindow *keyWindow = WGEKeyWindow();
+    for (UIWindow *w in WGEAllWindows()) {
+        if (w == keyWindow) continue;
+        if (w.isHidden) continue;
+
+        NSMutableArray<UIViewController *> *chain = [NSMutableArray array];
+        UIViewController *vc = w.rootViewController;
+        if (vc) [chain addObject:vc];
+        while (vc.presentedViewController) {
+            vc = vc.presentedViewController;
+            [chain addObject:vc];
+        }
+
+        BOOL isPasscodeWindow = NO;
+        for (UIViewController *candidate in chain) {
+            NSString *className = NSStringFromClass([candidate class]);
+            if ([className rangeOfString:@"Passcode"].location != NSNotFound) {
+                isPasscodeWindow = YES;
+                break;
+            }
+        }
+
+        NSString *windowClassName = NSStringFromClass([w class]);
+        if ([windowClassName rangeOfString:@"Passcode"].location != NSNotFound) {
+            isPasscodeWindow = YES;
+        }
+
+        if (isPasscodeWindow) {
+            w.hidden = YES;
+        }
+    }
+}
+
+static void WGERunFullCleanup(void) {
+    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder)
+                                                to:nil
+                                              from:nil
+                                          forEvent:nil];
+
+    for (UIWindow *w in WGEAllWindows()) {
+        [w endEditing:YES];
+    }
+
+    WGEHideLeftoverPasscodeWindows();
+    WGEPostSyntheticKeyboardHide();
+
+    UIWindow *keyWindow = WGEKeyWindow();
+    [keyWindow setNeedsLayout];
+    [keyWindow layoutIfNeeded];
+}
+
+static void WGEScheduleCleanup(NSTimeInterval delay) {
+    dispatch_time_t when = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delay * NSEC_PER_SEC));
+    dispatch_after(when, dispatch_get_main_queue(), ^{
+        WGERunFullCleanup();
     });
 }
 
-@end
+static IMP gOrigPasscodeDealloc = NULL;
+static IMP gOrigPasscodeViewWillDisappear = NULL;
+static IMP gOrigPasscodeViewDidAppear = NULL;
+
+static void WGEPasscodeDealloc(id self, SEL _cmd) {
+    gWGEPasscodeRecentlyShown = YES;
+    WGEScheduleCleanup(0.05);
+    WGEScheduleCleanup(0.35);
+    if (gOrigPasscodeDealloc) {
+        ((void (*)(id, SEL))gOrigPasscodeDealloc)(self, _cmd);
+    }
+}
+
+static void WGEPasscodeViewWillDisappear(id self, SEL _cmd, BOOL animated) {
+    if (gOrigPasscodeViewWillDisappear) {
+        ((void (*)(id, SEL, BOOL))gOrigPasscodeViewWillDisappear)(self, _cmd, animated);
+    }
+    gWGEPasscodeRecentlyShown = YES;
+    WGEScheduleCleanup(0.15);
+}
+
+static void WGEPasscodeViewDidAppear(id self, SEL _cmd, BOOL animated) {
+    if (gOrigPasscodeViewDidAppear) {
+        ((void (*)(id, SEL, BOOL))gOrigPasscodeViewDidAppear)(self, _cmd, animated);
+    }
+    gWGEPasscodeRecentlyShown = YES;
+}
+
+static void WGESwizzleInstanceMethod(Class cls, SEL selector, IMP replacement, IMP *originalOut) {
+    if (!cls) return;
+    Method method = class_getInstanceMethod(cls, selector);
+    if (!method) return;
+    *originalOut = method_getImplementation(method);
+    method_setImplementation(method, replacement);
+}
+
+__attribute__((constructor))
+static void WGEKeyboardFixInit(void) {
+    Class passcodeClass = NSClassFromString(@"_TtC10PasscodeUI23PasscodeEntryController");
+    if (passcodeClass) {
+        WGESwizzleInstanceMethod(passcodeClass, sel_registerName("dealloc"),
+                                  (IMP)WGEPasscodeDealloc, &gOrigPasscodeDealloc);
+        WGESwizzleInstanceMethod(passcodeClass, @selector(viewWillDisappear:),
+                                  (IMP)WGEPasscodeViewWillDisappear, &gOrigPasscodeViewWillDisappear);
+        WGESwizzleInstanceMethod(passcodeClass, @selector(viewDidAppear:),
+                                  (IMP)WGEPasscodeViewDidAppear, &gOrigPasscodeViewDidAppear);
+    }
+
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIApplicationDidBecomeActiveNotification
+                                                        object:nil
+                                                         queue:[NSOperationQueue mainQueue]
+                                                    usingBlock:^(NSNotification * _Nonnull note) {
+        if (gWGEPasscodeRecentlyShown) {
+            gWGEPasscodeRecentlyShown = NO;
+            WGEScheduleCleanup(0.05);
+            WGEScheduleCleanup(0.35);
+        }
+    }];
+}
