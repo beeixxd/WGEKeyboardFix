50292317c190245a1ce44b45deb0aaf4e7249701
beeieed <beeieed@hotmail.com>
2026-07-13 23:23:38 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,53 +1,8 @@
 #import <UIKit/UIKit.h>
-#import <objc/runtime.h>
 
 static BOOL gWGEAppTransitionActive = NO;
-static BOOL gWGEUserIsInteracting = NO;
 static BOOL gWGEAppIsLockedState = NO;
 static BOOL gWGEIsAppLockScreenShowing = YES;
-static UIWindow *gWGEGuardWindow = nil;
-static UITextField *gWGEGuardField = nil;
-
-@interface UIView (WGEKeyboardFix)
-- (BOOL)wge_becomeFirstResponder;
-@end
-
-@implementation UIView (WGEKeyboardFix)
-
-- (BOOL)wge_becomeFirstResponder {
-    if (gWGEIsAppLockScreenShowing || self == gWGEGuardField) {
-        return [self wge_becomeFirstResponder];
-    }
-    if (gWGEAppIsLockedState || gWGEAppTransitionActive) {
-        return [self wge_becomeFirstResponder];
-    }
-    if (!gWGEUserIsInteracting) {
-        return NO;
-    }
-    return [self wge_becomeFirstResponder];
-}
-
-@end
-
-@interface UIWindow (WGEKeyboardFix)
-- (void)wge_sendEvent:(UIEvent *)event;
-@end
-
-@implementation UIWindow (WGEKeyboardFix)
-
-- (void)wge_sendEvent:(UIEvent *)event {
-    if (event && event.type == UIEventTypeTouches) {
-        gWGEUserIsInteracting = YES;
-    }
-    [self wge_sendEvent:event];
-    if (event && event.type == UIEventTypeTouches) {
-        dispatch_async(dispatch_get_main_queue(), ^{
-            gWGEUserIsInteracting = NO;
-        });
-    }
-}
-
-@end
 
 @interface WGEKeyboardUltimatePerfectFixer : NSObject
 @end
@@ -60,20 +15,6 @@ static UITextField *gWGEGuardField = nil;
     dispatch_once(&onceToken, ^{
         fixer = [[WGEKeyboardUltimatePerfectFixer alloc] init];
         
-        Class viewClass = [UIView class];
-        Method m1 = class_getInstanceMethod(viewClass, @selector(becomeFirstResponder));
-        Method m2 = class_getInstanceMethod(viewClass, @selector(wge_becomeFirstResponder));
-        if (m1 && m2) {
-            method_exchangeImplementations(m1, m2);
-        }
-        
-        Class windowClass = [UIWindow class];
-        Method m3 = class_getInstanceMethod(windowClass, @selector(sendEvent:));
-        Method m4 = class_getInstanceMethod(windowClass, @selector(wge_sendEvent:));
-        if (m3 && m4) {
-            method_exchangeImplementations(m3, m4);
-        }
-        
         dispatch_async(dispatch_get_main_queue(), ^{
             NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
             [center addObserver:fixer selector:@selector(onLockOrBackground) name:UIApplicationDidEnterBackgroundNotification object:nil];
@@ -81,13 +22,6 @@ static UITextField *gWGEGuardField = nil;
             [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationWillEnterForegroundNotification object:nil];
             [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationDidBecomeActiveNotification object:nil];
             [center addObserver:fixer selector:@selector(onAppUnlockSuccess) name:@"WGEAppUnlockScreenDidDismissNotification" object:nil];
-            
-            gWGEGuardWindow = [[UIWindow alloc] initWithFrame:CGRectZero];
-            gWGEGuardWindow.windowLevel = -1.0;
-            gWGEGuardWindow.hidden = YES;
-            
-            gWGEGuardField = [[UITextField alloc] initWithFrame:CGRectZero];
-            [gWGEGuardWindow addSubview:gWGEGuardField];
         });
     });
 }
@@ -106,27 +40,66 @@ static UITextField *gWGEGuardField = nil;
 }
 
 - (void)onAppUnlockSuccess {
-    UIWindow *previousKeyWindow = [UIApplication sharedApplication].keyWindow;
+    gWGEIsAppLockScreenShowing = NO;
     
-    if (gWGEGuardWindow && gWGEGuardField) {
-        gWGEGuardWindow.hidden = NO;
-        [gWGEGuardWindow makeKeyWindow];
-        [gWGEGuardField wge_becomeFirstResponder];
-        [gWGEGuardField resignFirstResponder];
-        gWGEGuardWindow.hidden = YES;
-    }
+    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
     
-    if (previousKeyWindow) {
-        [previousKeyWindow makeKeyWindow];
-        [previousKeyWindow endEditing:YES];
-    }
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        gWGEIsAppLockScreenShowing = NO;
-        if (previousKeyWindow) {
-            [previousKeyWindow endEditing:YES];
+    NSMutableArray *allWindows = [NSMutableArray array];
+    if (@available(iOS 13.0, *)) {
+        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+            if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
+            for (UIWindow *w in ((UIWindowScene *)scene).windows) {
+                if (w) [allWindows addObject:w];
+            }
         }
-    });
+    }
+    if (allWindows.count == 0) {
+        NSArray *legacyWindows = [UIApplication sharedApplication].windows;
+        if (legacyWindows) [allWindows addObjectsFromArray:legacyWindows];
+    }
+    
+    for (UIWindow *w in allWindows) {
+        [w endEditing:YES];
+        NSString *className = NSStringFromClass([w class]);
+        if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+            w.hidden = YES;
+            w.alpha = 0.0;
+            CGRect frame = w.frame;
+            frame.size.height = 0;
+            w.frame = frame;
+        }
+    }
+    
+    for (int i = 1; i <= 5; i++) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.08 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+            
+            NSMutableArray *delayedWindows = [NSMutableArray array];
+            if (@available(iOS 13.0, *)) {
+                for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                    if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
+                    for (UIWindow *w in ((UIWindowScene *)scene).windows) {
+                        if (w) [delayedWindows addObject:w];
+                    }
+                }
+            }
+            if (delayedWindows.count == 0) {
+                NSArray *legacyWindows = [UIApplication sharedApplication].windows;
+                if (legacyWindows) [delayedWindows addObjectsFromArray:legacyWindows];
+            }
+            
+            for (UIWindow *w in delayedWindows) {
+                NSString *className = NSStringFromClass([w class]);
+                if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+                    w.hidden = YES;
+                    w.alpha = 0.0;
+                    CGRect frame = w.frame;
+                    frame.size.height = 0;
+                    w.frame = frame;
+                }
+            }
+        });
+    }
 }
 
 @end
