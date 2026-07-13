0b52af767cd090f0b3090ef523c415b8adf2fbb4
beeieed <beeieed@hotmail.com>
2026-07-13 23:48:37 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -17,63 +17,45 @@
 }
 
 - (void)onAppUnlockSuccess {
-    NSMutableArray *allWindows = [NSMutableArray array];
-    if (@available(iOS 13.0, *)) {
-        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
-            for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-                if (w) [allWindows addObject:w];
-            }
-        }
-    }
-    if (allWindows.count == 0) {
-        NSArray *legacyWindows = [UIApplication sharedApplication].windows;
-        if (legacyWindows) [allWindows addObjectsFromArray:legacyWindows];
-    }
-    
-    for (UIWindow *w in allWindows) {
-        NSString *className = NSStringFromClass([w class]);
-        if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
-            [UIView animateWithDuration:0.25 animations:^{
-                w.alpha = 0.0;
-            } completion:^(BOOL finished) {
-                w.hidden = YES;
-                w.alpha = 1.0;
-            }];
-        }
-    }
-    
-    for (int i = 1; i <= 4; i++) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.08 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            NSMutableArray *delayedWindows = [NSMutableArray array];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        UIWindow *activeWindow = [UIApplication sharedApplication].keyWindow;
+        if (!activeWindow) {
+            NSArray *windows = nil;
             if (@available(iOS 13.0, *)) {
                 for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                    if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
-                    for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-                        if (w) [delayedWindows addObject:w];
+                    if ([scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) {
+                        windows = [((UIWindowScene *)scene) windows];
+                        break;
                     }
                 }
             }
-            if (delayedWindows.count == 0) {
-                NSArray *legacyWindows = [UIApplication sharedApplication].windows;
-                if (legacyWindows) [delayedWindows addObjectsFromArray:legacyWindows];
+            if (!windows || windows.count == 0) {
+                windows = [UIApplication sharedApplication].windows;
             }
+            for (UIWindow *w in windows) {
+                if (w.isKeyWindow) {
+                    activeWindow = w;
+                    break;
+                }
+            }
+            if (!activeWindow && windows.count > 0) {
+                activeWindow = windows.firstObject;
+            }
+        }
+        
+        if (activeWindow) {
+            UITextField *shadowGuardField = [[UITextField alloc] initWithFrame:CGRectZero];
+            shadowGuardField.hidden = YES;
+            [activeWindow addSubview:shadowGuardField];
             
-            for (UIWindow *w in delayedWindows) {
-                NSString *className = NSStringFromClass([w class]);
-                if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
-                    if (w.hidden == NO && w.alpha > 0.0) {
-                        [UIView animateWithDuration:0.15 animations:^{
-                            w.alpha = 0.0;
-                        } completion:^(BOOL finished) {
-                            w.hidden = YES;
-                            w.alpha = 1.0;
-                        }];
-                    }
-                }
-            }
-        });
-    }
+            [shadowGuardField becomeFirstResponder];
+            
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.02 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                [shadowGuardField resignFirstResponder];
+                [shadowGuardField removeFromSuperview];
+            });
+        }
+    });
 }
 
 @end
