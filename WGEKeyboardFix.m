fd402dd830b96caa83f300acf9705d57be5b0d6b
beeieed <beeieed@hotmail.com>
2026-07-14 00:07:42 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,57 +1,77 @@
 #import <UIKit/UIKit.h>
 
-static id gWGEFixerNotificationObserver = nil;
-
-static void WGEAppUnlockScreenDidDismissHandler(CFNotificationCenterRef center, void *observer, CFStringRef name, const void *object, CFDictionaryRef userInfo) {
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIApplication *app = [UIApplication sharedApplication];
-        id window = nil;
-        
-        if ([app respondsToSelector:@selector(keyWindow)]) {
-            window = [app performSelector:@selector(keyWindow)];
-        }
-        
-        if (!window && [app respondsToSelector:@selector(windows)]) {
-            NSArray *allWindows = [app performSelector:@selector(windows)];
-            if (allWindows && allWindows.count > 0) {
-                window = allWindows.firstObject;
-            }
-        }
-        
-        if (window) {
-            Class tfClass = NSClassFromString(@"UITextField");
-            if (!tfClass) return;
-            
-            id shadowGuardField = [[tfClass alloc] init];
-            if (!shadowGuardField) return;
-            
-            if ([window respondsToSelector:@selector(addSubview:)]) {
-                [window performSelector:@selector(addSubview:) withObject:shadowGuardField];
-            }
-            
-            if ([shadowGuardField respondsToSelector:@selector(becomeFirstResponder)]) {
-                [shadowGuardField performSelector:@selector(becomeFirstResponder)];
-            }
-            
-            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.02 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                if ([shadowGuardField respondsToSelector:@selector(resignFirstResponder)]) {
-                    [shadowGuardField performSelector:@selector(resignFirstResponder)];
+static void WGEHandleUnlockShadowSuppression(void) {
+    UIApplication *app = [UIApplication sharedApplication];
+    NSArray *allWindows = nil;
+    
+    if ([app respondsToSelector:@selector(windows)]) {
+        allWindows = [app performSelector:@selector(windows)];
+    }
+    
+    if (!allWindows || allWindows.count == 0) {
+        if (@available(iOS 13.0, *)) {
+            NSMutableArray *sceneWindows = [NSMutableArray array];
+            for (id scene in [app performSelector:@selector(connectedScenes)]) {
+                if ([[scene class] isKindOfClass:NSClassFromString(@"UIWindowScene")]) {
+                    NSArray *wins = [scene performSelector:@selector(windows)];
+                    if (wins) [sceneWindows addObjectsFromArray:wins];
                 }
-                if ([shadowGuardField respondsToSelector:@selector(removeFromSuperview)]) {
-                    [shadowGuardField performSelector:@selector(removeFromSuperview)];
+            }
+            allWindows = sceneWindows;
+        }
+    }
+    
+    for (id w in allWindows) {
+        NSString *className = NSStringFromClass([w class]);
+        if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+            if ([w respondsToSelector:@selector(setWindowLevel:)]) {
+                NSMethodSignature *sig = [w methodSignatureForSelector:@selector(setWindowLevel:)];
+                if (sig) {
+                    NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+                    [inv setSelector:@selector(setWindowLevel:)];
+                    [inv setTarget:w];
+                    double lowLevel = -1.0;
+                    [inv setArgument:&lowLevel atIndex:2];
+                    [inv invoke];
                 }
-            });
+            }
+        }
+    }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        NSArray *restoreWindows = nil;
+        if ([app respondsToSelector:@selector(windows)]) {
+            restoreWindows = [app performSelector:@selector(windows)];
+        }
+        for (id w in restoreWindows) {
+            NSString *className = NSStringFromClass([w class]);
+            if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+                if ([w respondsToSelector:@selector(setWindowLevel:)]) {
+                    NSMethodSignature *sig = [w methodSignatureForSelector:@selector(setWindowLevel:)];
+                    if (sig) {
+                        NSInvocation *inv = [NSInvocation invocationWithMethodSignature:sig];
+                        [inv setSelector:@selector(setWindowLevel:)];
+                        [inv setTarget:w];
+                        double normalLevel = 10.0;
+                        if ([className containsString:@"Keyboard"]) {
+                            normalLevel = 1000.0;
+                        }
+                        [inv setArgument:&normalLevel atIndex:2];
+                        [inv invoke];
+                    }
+                }
+            }
         }
     });
 }
 
-__attribute__((constructor)) static void initializeWGEKeyboardFixerPlugin(void) {
+__attribute__((constructor)) static void initializeWGEKeyboardShadowZIndexFixer(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
         [[NSNotificationCenter defaultCenter] addObserverForName:@"WGEAppUnlockScreenDidDismissNotification"
                                                           object:nil
                                                            queue:[NSOperationQueue mainQueue]
                                                       usingBlock:^(NSNotification * _Nonnull note) {
-            WGEAppUnlockScreenDidDismissHandler(NULL, NULL, NULL, NULL, NULL);
+            WGEHandleUnlockShadowSuppression();
         }];
     });
 }
