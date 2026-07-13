1f5413a4631b1f078fb1bc1f0be578fffc2144bc
beeieed <beeieed@hotmail.com>
2026-07-13 23:50:26 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -18,36 +18,11 @@
 
 - (void)onAppUnlockSuccess {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        UIWindow *activeWindow = [UIApplication sharedApplication].keyWindow;
-        if (!activeWindow) {
-            NSArray *windows = nil;
-            if (@available(iOS 13.0, *)) {
-                for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-                    if ([scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) {
-                        windows = [((UIWindowScene *)scene) windows];
-                        break;
-                    }
-                }
-            }
-            if (!windows || windows.count == 0) {
-                windows = [UIApplication sharedApplication].windows;
-            }
-            for (UIWindow *w in windows) {
-                if (w.isKeyWindow) {
-                    activeWindow = w;
-                    break;
-                }
-            }
-            if (!activeWindow && windows.count > 0) {
-                activeWindow = windows.firstObject;
-            }
-        }
-        
+        UIWindow *activeWindow = [[UIApplication sharedApplication] keyWindow];
         if (activeWindow) {
             UITextField *shadowGuardField = [[UITextField alloc] initWithFrame:CGRectZero];
             shadowGuardField.hidden = YES;
             [activeWindow addSubview:shadowGuardField];
-            
             [shadowGuardField becomeFirstResponder];
             
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.02 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
