e65e580a3429d5eda02bfadee50debaefd5ceb03
beeieed <beeieed@hotmail.com>
2026-07-13 23:54:54 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,4 +1,6 @@
 #import <UIKit/UIKit.h>
+#import <objc/runtime.h>
+#import <objc/message.h>
 
 @interface WGEKeyboardUltimatePerfectFixer : NSObject
 @end
@@ -20,22 +22,44 @@
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.05 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIApplication *app = [UIApplication sharedApplication];
         UIWindow *window = nil;
+        
         if ([app respondsToSelector:@selector(keyWindow)]) {
             window = [app performSelector:@selector(keyWindow)];
         }
+        
         if (!window && [app respondsToSelector:@selector(windows)]) {
             NSArray *allWindows = [app performSelector:@selector(windows)];
             if (allWindows && allWindows.count > 0) {
                 window = allWindows.firstObject;
             }
         }
+        
         if (window) {
-            UITextField *shadowGuardField = [[UITextField alloc] initWithFrame:CGRectZero];
-            [window addSubview:shadowGuardField];
-            [shadowGuardField becomeFirstResponder];
+            Class tfClass = objc_getClass("UITextField");
+            if (!tfClass) return;
+            
+            id shadowGuardField = [[tfClass alloc] init];
+            if (!shadowGuardField) return;
+            
+            if ([shadowGuardField respondsToSelector:@selector(setFrame:)]) {
+                ((void (*)(id, SEL, CGRect))objc_msgSend)(shadowGuardField, @selector(setFrame:), CGRectZero);
+            }
+            
+            if ([window respondsToSelector:@selector(addSubview:)]) {
+                ((void (*)(id, SEL, id))objc_msgSend)(window, @selector(addSubview:), shadowGuardField);
+            }
+            
+            if ([shadowGuardField respondsToSelector:@selector(becomeFirstResponder)]) {
+                ((BOOL (*)(id, SEL))objc_msgSend)(shadowGuardField, @selector(becomeFirstResponder));
+            }
+            
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.02 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                [shadowGuardField resignFirstResponder];
-                [shadowGuardField removeFromSuperview];
+                if ([shadowGuardField respondsToSelector:@selector(resignFirstResponder)]) {
+                    ((BOOL (*)(id, SEL))objc_msgSend)(shadowGuardField, @selector(resignFirstResponder));
+                }
+                if ([shadowGuardField respondsToSelector:@selector(removeFromSuperview)]) {
+                    ((void (*)(id, SEL))objc_msgSend)(shadowGuardField, @selector(removeFromSuperview));
+                }
             });
         }
     });
