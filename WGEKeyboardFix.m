906334619234fcb148383d9942a0014ec2d21609
beeieed <beeieed@hotmail.com>
2026-07-13 23:15:30 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -5,74 +5,21 @@ static BOOL gWGEAppTransitionActive = NO;
 static BOOL gWGEUserIsInteracting = NO;
 static BOOL gWGEAppIsLockedState = NO;
 static BOOL gWGEIsAppLockScreenShowing = YES;
-
-static NSArray<UIWindow *> *WGEAllWindows(void) {
-    NSMutableArray<UIWindow *> *result = [NSMutableArray array];
-    if (@available(iOS 13.0, *)) {
-        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
-            UIWindowScene *ws = (UIWindowScene *)scene;
-            for (UIWindow *w in ws.windows) {
-                if (w) [result addObject:w];
-            }
-        }
-    }
-    if (result.count == 0) {
-        NSArray *legacyWindows = [UIApplication sharedApplication].windows;
-        if (legacyWindows) [result addObjectsFromArray:legacyWindows];
-    }
-    return result;
-}
-
-static void WGERunFullCleanup(void) {
-    if (gWGEAppIsLockedState) {
-        return;
-    }
-    
-    if (!gWGEIsAppLockScreenShowing && !gWGEUserIsInteracting) {
-        [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
-    }
-
-    for (UIWindow *w in WGEAllWindows()) {
-        NSString *windowClassName = NSStringFromClass([w class]);
-        if ([windowClassName rangeOfString:@"Passcode"].location != NSNotFound ||
-            [windowClassName rangeOfString:@"Secure"].location != NSNotFound) {
-            continue;
-        }
-
-        BOOL isKeyboardWindow = [windowClassName containsString:@"TextEffects"] || [windowClassName containsString:@"Keyboard"];
-        if (isKeyboardWindow) {
-            if (!gWGEIsAppLockScreenShowing && !gWGEUserIsInteracting) {
-                w.hidden = YES;
-                w.alpha = 0.0;
-                [w.rootViewController.view endEditing:YES];
-                for (UIView *subview in [w subviews]) {
-                    subview.hidden = YES;
-                    subview.alpha = 0.0;
-                }
-            } else if (gWGEIsAppLockScreenShowing && w.frame.size.height < 100) {
-                w.alpha = 0.0;
-            }
-        }
-    }
-}
+static UIWindow *gWGEGuardWindow = nil;
+static UITextField *gWGEGuardField = nil;
 
 static BOOL (*orig_becomeFirstResponder)(id, SEL);
 static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
-    if (gWGEIsAppLockScreenShowing) {
-        return orig_becomeFirstResponder(self, _cmd);
+    if (gWGEIsAppLockScreenShowing || self == gWGEGuardField) {
+        return ((BOOL(*)(id, SEL))orig_becomeFirstResponder)(self, _cmd);
     }
-    
     if (gWGEAppIsLockedState || gWGEAppTransitionActive) {
-        return orig_becomeFirstResponder(self, _cmd);
+        return ((BOOL(*)(id, SEL))orig_becomeFirstResponder)(self, _cmd);
     }
-    
     if (!gWGEUserIsInteracting) {
-        WGERunFullCleanup();
         return NO;
     }
-    
-    return orig_becomeFirstResponder(self, _cmd);
+    return ((BOOL(*)(id, SEL))orig_becomeFirstResponder)(self, _cmd);
 }
 
 static void (*orig_windowSendEvent)(id, SEL, UIEvent *);
@@ -80,7 +27,7 @@ static void new_windowSendEvent(id self, SEL _cmd, UIEvent *event) {
     if (event && event.type == UIEventTypeTouches) {
         gWGEUserIsInteracting = YES;
     }
-    orig_windowSendEvent(self, _cmd, event);
+    ((void(*)(id, SEL, UIEvent *))orig_windowSendEvent)(self, _cmd, event);
     if (event && event.type == UIEventTypeTouches) {
         dispatch_async(dispatch_get_main_queue(), ^{
             gWGEUserIsInteracting = NO;
@@ -102,14 +49,14 @@ static void new_windowSendEvent(id self, SEL _cmd, UIEvent *event) {
         Class viewClass = [UIView class];
         Method m1 = class_getInstanceMethod(viewClass, @selector(becomeFirstResponder));
         if (m1) {
-            orig_becomeFirstResponder = (void *)method_getImplementation(m1);
+            orig_becomeFirstResponder = (BOOL(*)(id, SEL))method_getImplementation(m1);
             method_setImplementation(m1, (IMP)new_becomeFirstResponder);
         }
         
         Class windowClass = [UIWindow class];
         Method m2 = class_getInstanceMethod(windowClass, @selector(sendEvent:));
         if (m2) {
-            orig_windowSendEvent = (void *)method_getImplementation(m2);
+            orig_windowSendEvent = (void(*)(id, SEL, UIEvent *))method_getImplementation(m2);
             method_setImplementation(m2, (IMP)new_windowSendEvent);
         }
         
@@ -119,8 +66,24 @@ static void new_windowSendEvent(id self, SEL _cmd, UIEvent *event) {
             [center addObserver:fixer selector:@selector(onLockOrBackground) name:UIApplicationWillResignActiveNotification object:nil];
             [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationWillEnterForegroundNotification object:nil];
             [center addObserver:fixer selector:@selector(onUnlockOrActive) name:UIApplicationDidBecomeActiveNotification object:nil];
-            
             [center addObserver:fixer selector:@selector(onAppUnlockSuccess) name:@"WGEAppUnlockScreenDidDismissNotification" object:nil];
+            
+            if (@available(iOS 13.0, *)) {
+                for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                    if ([scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) {
+                        gWGEGuardWindow = [[UIWindow alloc] initWithWindowScene:(UIWindowScene *)scene];
+                        break;
+                    }
+                }
+            }
+            if (!gWGEGuardWindow) {
+                gWGEGuardWindow = [[UIWindow alloc] initWithFrame:CGRectZero];
+            }
+            gWGEGuardWindow.windowLevel = UIWindowLevelNormal - 1;
+            gWGEGuardWindow.hidden = YES;
+            
+            gWGEGuardField = [[UITextField alloc] initWithFrame:CGRectZero];
+            [gWGEGuardWindow addSubview:gWGEGuardField];
         });
     });
 }
@@ -135,52 +98,31 @@ static void new_windowSendEvent(id self, SEL _cmd, UIEvent *event) {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         gWGEAppTransitionActive = NO;
         gWGEAppIsLockedState = NO;
-        WGERunFullCleanup();
     });
 }
 
 - (void)onAppUnlockSuccess {
-    UIWindow *keyWindow = [UIApplication sharedApplication].keyWindow;
-    if (keyWindow) {
-        UITextField *guardField = [[UITextField alloc] initWithFrame:CGRectZero];
-        guardField.hidden = YES;
-        [keyWindow addSubview:guardField];
-        
-        BOOL isShowingState = gWGEIsAppLockScreenShowing;
-        gWGEIsAppLockScreenShowing = YES;
-        [guardField becomeFirstResponder];
-        gWGEIsAppLockScreenShowing = isShowingState;
-        
-        [guardField resignFirstResponder];
-        [guardField removeFromSuperview];
-    }
-
-    [[UIApplication sharedApplication] sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
+    UIWindow *previousKeyWindow = [UIApplication sharedApplication].keyWindow;
     
-    if (@available(iOS 13.0, *)) {
-        for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
-            if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
-            for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-                [w endEditing:YES];
-            }
-        }
-    } else {
-        [[UIApplication sharedApplication].keyWindow endEditing:YES];
-        for (UIWindow *w in [UIApplication sharedApplication].windows) {
-            [w endEditing:YES];
-        }
+    if (gWGEGuardWindow && gWGEGuardField) {
+        gWGEGuardWindow.hidden = NO;
+        [gWGEGuardWindow makeKeyWindow];
+        [gWGEGuardField becomeFirstResponder];
+        [gWGEGuardField resignFirstResponder];
+        gWGEGuardWindow.hidden = YES;
     }
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.12 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    if (previousKeyWindow) {
+        [previousKeyWindow makeKeyWindow];
+        [previousKeyWindow endEditing:YES];
+    }
+    
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.1 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         gWGEIsAppLockScreenShowing = NO;
-        WGERunFullCleanup();
+        if (previousKeyWindow) {
+            [previousKeyWindow endEditing:YES];
+        }
     });
-    
-    for (int i = 1; i <= 6; i++) {
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)((1 + i) * 0.08 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-            WGERunFullCleanup();
-        });
-    }
 }
 
 @end
