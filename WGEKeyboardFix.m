834e8d8136169e25af0f0a3bec3c2c3e6b2a3cab
beeieed <beeieed@hotmail.com>
2026-07-13 23:30:03 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,7 +1,6 @@
 - (void)handleUnlockSuccessAndDismissSafely {
     self.view.alpha = 0.0;
     
-    [self.passwordTextField resignFirstResponder];
     [self.view endEditing:YES];
     
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.35 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
