a903ee47037f2750e4554696580e58ffba86603b
beeieed <beeieed@hotmail.com>
2026-07-13 23:44:04 +0700

    Update WGEKeyboardFix.m

--- WGEKeyboardFix.m
+++ WGEKeyboardFix.m
@@ -1,15 +1,4 @@
 #import <UIKit/UIKit.h>
-#import <objc/runtime.h>
-
-static BOOL gWGEBlockKeyboardToPreventShadow = NO;
-
-static BOOL (*orig_becomeFirstResponder)(id, SEL);
-static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
-    if (gWGEBlockKeyboardToPreventShadow) {
-        return NO;
-    }
-    return ((BOOL(*)(id, SEL))orig_becomeFirstResponder)(self, _cmd);
-}
 
 @interface WGEKeyboardUltimatePerfectFixer : NSObject
 @end
@@ -21,14 +10,6 @@ static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
         fixer = [[WGEKeyboardUltimatePerfectFixer alloc] init];
-        
-        Class viewClass = [UIView class];
-        Method m1 = class_getInstanceMethod(viewClass, @selector(becomeFirstResponder));
-        if (m1) {
-            orig_becomeFirstResponder = (BOOL(*)(id, SEL))method_getImplementation(m1);
-            method_setImplementation(m1, (IMP)new_becomeFirstResponder);
-        }
-        
         dispatch_async(dispatch_get_main_queue(), ^{
             [[NSNotificationCenter defaultCenter] addObserver:fixer selector:@selector(onAppUnlockSuccess) name:@"WGEAppUnlockScreenDidDismissNotification" object:nil];
         });
@@ -36,22 +17,63 @@ static BOOL new_becomeFirstResponder(id self, SEL _cmd) {
 }
 
 - (void)onAppUnlockSuccess {
-    gWGEBlockKeyboardToPreventShadow = YES;
-    
+    NSMutableArray *allWindows = [NSMutableArray array];
     if (@available(iOS 13.0, *)) {
         for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
             if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
             for (UIWindow *w in ((UIWindowScene *)scene).windows) {
-                [w endEditing:YES];
+                if (w) [allWindows addObject:w];
             }
         }
-    } else {
-        [[UIApplication sharedApplication].keyWindow endEditing:YES];
+    }
+    if (allWindows.count == 0) {
+        NSArray *legacyWindows = [UIApplication sharedApplication].windows;
+        if (legacyWindows) [allWindows addObjectsFromArray:legacyWindows];
     }
     
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        gWGEBlockKeyboardToPreventShadow = NO;
-    });
+    for (UIWindow *w in allWindows) {
+        NSString *className = NSStringFromClass([w class]);
+        if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+            [UIView animateWithDuration:0.25 animations:^{
+                w.alpha = 0.0;
+            } completion:^(BOOL finished) {
+                w.hidden = YES;
+                w.alpha = 1.0;
+            }];
+        }
+    }
+    
+    for (int i = 1; i <= 4; i++) {
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(i * 0.08 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            NSMutableArray *delayedWindows = [NSMutableArray array];
+            if (@available(iOS 13.0, *)) {
+                for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
+                    if (![scene isKindOfClass:NSClassFromString(@"UIWindowScene")]) continue;
+                    for (UIWindow *w in ((UIWindowScene *)scene).windows) {
+                        if (w) [delayedWindows addObject:w];
+                    }
+                }
+            }
+            if (delayedWindows.count == 0) {
+                NSArray *legacyWindows = [UIApplication sharedApplication].windows;
+                if (legacyWindows) [delayedWindows addObjectsFromArray:legacyWindows];
+            }
+            
+            for (UIWindow *w in delayedWindows) {
+                NSString *className = NSStringFromClass([w class]);
+                if ([className containsString:@"TextEffects"] || [className containsString:@"Keyboard"]) {
+                    if (w.hidden == NO && w.alpha > 0.0) {
+                        [UIView animateWithDuration:0.15 animations:^{
+                            w.alpha = 0.0;
+                        } completion:^(BOOL finished) {
+                            w.hidden = YES;
+                            w.alpha = 1.0;
+                        }];
+                    }
+                }
+            }
+        });
+    }
 }
 
 @end
